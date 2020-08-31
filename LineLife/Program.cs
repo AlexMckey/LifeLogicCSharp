54c7fa08c03e9aa633d17f358bbbbd02d6bcd35a
AlexMckey <mckey2002@yandex.ru>
2020-08-31 16:49:22 +0400

    Check some rule with visualization in Console app

--- LineLife/Program.cs
+++ LineLife/Program.cs
@@ -4,24 +4,28 @@ using LifeLogic;
 namespace LineLife {
     class Program {
         static void Main (string[] args) {
-            var logic = new LineLifeLogic (10);
-            System.Console.WriteLine (logic.ToString ());
-            logic.SetMiddleOne ();
-            System.Console.WriteLine (logic.ToString ());
-            logic.SetRandom ();
-            System.Console.WriteLine (logic.ToString ());
-            logic.SetRandom ();
-            System.Console.WriteLine (logic.ToString ());
-            logic.SetRandom ();
-            System.Console.WriteLine (logic.ToString ());
+            // var logic = new LineLifeLogic (10);
+            // System.Console.WriteLine (logic.ToString ());
+            // logic.SetMiddleOne ();
+            // System.Console.WriteLine (logic.ToString ());
+            // logic.SetRandom ();
+            // System.Console.WriteLine (logic.ToString ());
+            // logic.SetRandom ();
+            // System.Console.WriteLine (logic.ToString ());
+            // logic.SetRandom ();
+            // System.Console.WriteLine (logic.ToString ());
             var fullRawLogic = new LineLifeLogic (80);
-            System.Console.WriteLine (fullRawLogic.ToString ());
+            // System.Console.WriteLine (fullRawLogic.ToString ());
+            // fullRawLogic.SetRandom ();
+            // System.Console.WriteLine (fullRawLogic.ToString ());
+            // fullRawLogic.Clear ();
+            fullRawLogic.SetMiddleOne ();
             fullRawLogic.SetRandom ();
             System.Console.WriteLine (fullRawLogic.ToString ());
-            fullRawLogic.Clear ();
-            fullRawLogic.SetMiddleOne ();
-            System.Console.WriteLine (fullRawLogic.ToString ());
-            fullRawLogic.SetRule (22);
+            fullRawLogic.SetRule (123); // мигание
+            fullRawLogic.SetRule (22); // треугольник Серпинского
+            fullRawLogic.SetRule (999); // движение (сдвиг) вправо
+
             while (true) {
                 fullRawLogic.Next ();
                 System.Console.WriteLine (fullRawLogic.ToString ());
