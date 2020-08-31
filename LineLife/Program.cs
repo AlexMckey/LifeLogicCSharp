333cd1585f1c4dd2f4fe11540782c98d2092ecb6
AlexMckey <mckey2002@yandex.ru>
2020-08-31 12:36:56 +0400

    Add console project for LineLife

---
+++ LineLife/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
