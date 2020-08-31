7f36ac7fbe4ff45ac9fd9324f996412ba54d3e80
AlexMckey <mckey2002@yandex.ru>
2020-08-31 12:37:37 +0400

    Add library for LifeLogic

---
+++ LifeLogic/LineLifeLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LifeLogic {
+    public class LineLifeLogic {
+        int size;
+        bool[] arr;
+        bool[] rule = new bool[8];
+
+        public LineLifeLogic (int size) {
+            this.size = size;
+            this.arr = new bool[size];
+        }
+
+        public LineLifeLogic (bool[] bools) {
+            this.size = bools.Length;
+            this.arr = bools;
+        }
+
+        public SetRandom () {
+            Random random = new Random ();
+            foreach (var v in arr) {
+                v = random.Next (2) == 1;
+            }
+        }
+    }
+}
