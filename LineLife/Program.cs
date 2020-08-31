1abe0cb0a54b433bcce4cb466f37ecde9f05fdab
AlexMckey <mckey2002@yandex.ru>
2020-08-31 14:11:42 +0400

    Check console app with long logic size = 80

--- LineLife/Program.cs
+++ LineLife/Program.cs
@@ -14,6 +14,12 @@ namespace LineLife {
             System.Console.WriteLine (logic.ToString ());
             logic.SetRandom ();
             System.Console.WriteLine (logic.ToString ());
+            var fullRawLogic = new LineLifeLogic (80);
+            System.Console.WriteLine (fullRawLogic.ToString ());
+            fullRawLogic.SetMiddleOne ();
+            System.Console.WriteLine (fullRawLogic.ToString ());
+            fullRawLogic.SetRandom ();
+            System.Console.WriteLine (fullRawLogic.ToString ());
         }
     }
 }
